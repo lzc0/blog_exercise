1c2dd75990d424649a880873944c19d5140176b5
lzc0 <zyxlzc@126.com>
2022-02-13 22:25:43 +0800

    短信验证码

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,10 +1,12 @@
 #进行users子应用的视图路由
 from django.urls import path
-from users.views import RegisterView, ImageCodeView
+from users.views import RegisterView, ImageCodeView, SmsCodeView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     #图片验证码的路由
-    path('imagecode/', ImageCodeView.as_view(), name='imagecode')
+    path('imagecode/', ImageCodeView.as_view(), name='imagecode'),
+    #短信发送
+    path('smscode/', SmsCodeView.as_view(), name='smscode'),
 ]
 
--- blog/users/views.py
+++ blog/users/views.py
@@ -34,3 +34,41 @@ class ImageCodeView(View):
         redis_conn.setex('img:%s' % uuid, 300, text)
         #返回图片二进制
         return HttpResponse(image, content_type='image/jpeg')
+
+
+from django.http.response import JsonResponse
+from utils.response_code import RETCODE
+import logging
+logger = logging.getLogger('django')
+from random import randint
+from libs.yuntongxun.sms import CCP
+class SmsCodeView(View):
+
+    def get(self, request):
+
+        #接收参数
+        mobile = request.GET.get('mobile')
+        image_code = request.GET.get('image_code')
+        uuid = request.GET.get('uuid')
+        #参数的验证
+        if not all([mobile, image_code, uuid]):
+            return JsonResponse({'code': RETCODE.NECESSARYPARAMERR, 'errmsg': '缺少必要参数'})
+        redis_conn = get_redis_connection('default')
+        redis_image_code = redis_conn.get('img:%s' % uuid)
+        if redis_image_code is None:
+            return JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图片验证码已过期'})
+        try:
+            redis_conn.delete('img:%s' % uuid)
+        except Exception as e:
+            logger.error(e)
+        if redis_image_code.decode().lower() != image_code.lower():
+            return JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图片验证码错误'})
+        #生成短信验证码
+        sms_code = '%06d'% randint(0, 999999)
+        logger.info(sms_code)
+        #保存短信验证码到redis
+        redis_conn.setex('sms:%s' % mobile, 300, sms_code)
+        #发送短信
+        CCP().send_template_sms(mobile, [sms_code, 5], 1)
+        #返回响应
+        return JsonResponse({'code': RETCODE.OK, 'errmsg': '短信发送成功'})
